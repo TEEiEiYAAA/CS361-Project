e99fa29e61d34a9e087c2e0932c26fbed0e69ec1
Kullaset Netphet - 6609650194 <145306711+JJJOBJY@users.noreply.github.com>
2025-11-20 23:28:49 +0700

    Update detailed-activities.js

--- JS/detailed-activities.js
+++ JS/detailed-activities.js
@@ -1,6 +1,6 @@
 // Configuration
 const CONFIG = {
-  API_BASE_URL: 'https://mb252cstbb.execute-api.us-east-1.amazonaws.com/prod',
+  API_BASE_URL: 'https://jcxjc9ot0e.execute-api.us-east-1.amazonaws.com/prod',
   ENDPOINTS: {
     GET_ACTIVITY_DETAIL: '/activities/{activityId}',
     REGISTER_ACTIVITY: '/activities/register',
