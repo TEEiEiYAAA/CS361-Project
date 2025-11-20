91d95be2ced70269c95471b03e17d46f79dfac97
Kullaset Netphet - 6609650194 <145306711+JJJOBJY@users.noreply.github.com>
2025-11-20 23:24:21 +0700

    Update quiz-categories.js

--- JS/quiz-categories.js
+++ JS/quiz-categories.js
@@ -1,5 +1,5 @@
 const EXAM_LIST_URL = 'https://quiz-exam-data.s3.us-east-1.amazonaws.com/exams_list.json';
-const API_BASE_URL = 'https://mb252cstbb.execute-api.us-east-1.amazonaws.com/prod';
+const API_BASE_URL = 'https://jcxjc9ot0e.execute-api.us-east-1.amazonaws.com/prod';
 let allExams = [];
 
 // ============================================================
