6fe5239fca13b318fa01d425beb3e959e41c8bdc
Kullaset Netphet - 6609650194 <145306711+JJJOBJY@users.noreply.github.com>
2025-11-20 23:24:59 +0700

    Update my-students.js

--- JS/my-students.js
+++ JS/my-students.js
@@ -1,5 +1,5 @@
 // Configuration
-const API_BASE_URL = 'https://mb252cstbb.execute-api.us-east-1.amazonaws.com/prod';
+const API_BASE_URL = 'https://jcxjc9ot0e.execute-api.us-east-1.amazonaws.com/prod';
 const REQUIRED_ACTIVITIES_COUNT = 3;
 
 // Global variables
